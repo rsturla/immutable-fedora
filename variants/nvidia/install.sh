13eaa72188278333277cc4ead0438d6715cee21e
Robert Sturla <robertsturla@outlook.com>
2023-04-15 21:26:09 +0000

    Install xorg-x11-server-utils in nvidia images

--- variants/nvidia/install.sh
+++ variants/nvidia/install.sh
@@ -11,6 +11,7 @@ source /var/cache/akmods/nvidia-vars
 
 rpm-ostree install \
     xorg-x11-drv-${NVIDIA_PACKAGE_NAME}-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
+    xorg-x11-server-utils \
     nvidia-container-toolkit nvidia-vaapi-driver \
     /var/cache/akmods/${NVIDIA_PACKAGE_NAME}/kmod-${NVIDIA_PACKAGE_NAME}-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm \
     /tmp/nvidia-addons/rpmbuild/RPMS/noarch/nvidia-addons-*.rpm
